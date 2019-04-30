6a5b60e4fe501e6d6359870db9db46d8fc17b8f4
guixiangyu <guixiangyu1@gmail.com>
2019-04-30 10:38:53 +0800

    init

--- model/config.py
+++ model/config.py
@@ -95,7 +95,7 @@ class Config():
     train_embeddings = False
     nepochs          = 200
     dropout          = 0.5
-    batch_size       = 200
+    batch_size       = 100
     lr_method        = "adam"
     lr               = 0.01
     lr_decay         = 1.0
