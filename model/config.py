4738d39636114b1203afc03e5aa506145d606f6a
guixiangyu <guixiangyu1@gmail.com>
2019-04-30 09:13:15 +0800

    init

--- model/config.py
+++ model/config.py
@@ -74,7 +74,7 @@ class Config():
     filename_glove = "../gem_extractor/data/dewiki_20180420_300d.txt"
     # trimmed embeddings (created from glove_filename with build_data.py)
     filename_trimmed = "data/dewiki_20180420_300d.npz"
-    use_pretrained = True
+    use_pretrained = False
 
     # dataset
     filename_dev = "data/valid_gem.txt"
@@ -109,4 +109,4 @@ class Config():
 
     # NOTE: if both chars and crf, only 1.6x slower on GPU
     use_crf = False # if crf, training is 1.7x slower on CPU
-    use_chars = False # if char embedding, training is 3.5x slower on CPU
+    use_chars = True # if char embedding, training is 3.5x slower on CPU
